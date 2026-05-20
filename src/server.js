9661c2373f2b2875ad9c3dfe1555cb0ff5d46bcc
Manasa2204 <manasa220402@gmail.com>
2026-05-20 09:42:09 +0530

    modfiy cors origin

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ export const createServer = () => {
     .use(express.json())
     .use(
       cors({
-        origin: "https://inventory-mgmt-fe.vercel.app/",
+        origin: "https://inventory-mgmt-fe.vercel.app",
         credentials: true,
       }),
     );
